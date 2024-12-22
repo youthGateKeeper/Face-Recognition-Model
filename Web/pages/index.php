8e17b46750c17f1bf43419392bfe2ed592e86d9c
최도은 <choidoeun0102@gmail.com>
2024-12-22 13:01:13 +0900

    메인페이지 내용 추가

--- Web/pages/index.php
+++ Web/pages/index.php
@@ -4,12 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>청소년 게이트 키퍼</title>
+    <title>YOUTH GATE KEEPER</title>
     <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
-    <h1>메인페이지입니다.</h1>
+    <div class="mainPage">
+        <div>
+            <h1>YOUTH GATE KEEPER</h1>
+            <p>AI와 대화를 통해 기분전환을 해보아요!</p>
+            <h3>AI를 통해 사용자의 감정인식 기능이 가능해요!</h3>
+            <div class="main_img">
+                <img src=".../assets/sad.phg" alt="">
+                <img src=".../assets/sad.phg" alt="">
+                <img src=".../assets/sad.phg" alt="">
+            </div>
+            <h3>기분이 우울할 때, Gamini-Bot을 사용할 수 있어요!</h3>
+            <div class="main_img">
+                <img src="https://github.com/user-attachments/assets/7655037b-2d37-4fb3-8817-daecc0dbfe9c" alt="">
+            </div>
+        </div>
+    </div>
     <script src="../script.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
