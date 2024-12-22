0ecf118ec8b093e638bcaf4335322e4952a0c3b2
최도은 <choidoeun0102@gmail.com>
2024-12-22 11:59:30 +0900

    라우터 및 스타일 시트 위치 변경

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("./config/DBconnect.php");
+include("./Web/config/DBconnect.php");
 
 $request = $_SERVER['REQUEST_URI'];
 $path = explode("?", $request);
@@ -12,10 +12,10 @@ $page = "";
 if ($resource[1] == "api") {
     switch ($resource[2]) {
         case "register":
-            $page = "./api/register.php";
+            $page = "./Web/api/register.php";
             break;
             case "login":
-                $page = "./api/login.php";
+                $page = "./Web/api/login.php";
                 break;
         default:
             echo "잘못된 api 접근입니다.";
@@ -25,28 +25,28 @@ if ($resource[1] == "api") {
 } else {
     switch ($resource[1]) {
         case "":
-            $page = "./pages/index.php";
+            $page = "./Web/pages/index.php";
             break;
         case "login":
-            $page = "./pages/login.php";
+            $page = "./Web/pages/login.php";
             break;
         case "register":
-            $page = "./pages/register.php";
+            $page = "./Web/pages/register.php";
             break;
         case "myPage":
-            $page = "./pages/myPage.php";
+            $page = "./Web/pages/myPage.php";
             break;
         case "FaceRecognition":
-            $page = "./pages/FaceRecognition.php";
+            $page = "./Web/pages/FaceRecognition.php";
             break;
         case "emotionDiary":
-            $page = "./pages/emotionDiary.php";
+            $page = "./Web/pages/emotionDiary.php";
             break;
         default:
             echo "잘못된 접근입니다.";
             return 0;
     }
-    include("./componets/header.php");
+    include("./Web/componets/header.php");
     include $page;
-    include("./componets/footer.php");
+    include("./Web/componets/footer.php");
 }
